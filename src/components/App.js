c2a370858925900d9a9a9155ed177328613148a8
bizip <bizip04@gmail.com>
2022-10-02 00:02:48 +0200

    Develop display message functionalities

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import Message from './Message';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Message />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/redux/MessageSlice.js
@@ -0,0 +1,26 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  greeting: '',
+};
+
+export const getMessage = createAsyncThunk('greeting/getGreeting', async () => {
+  const message = await fetch('http://localhost:3000/api/v1/messages', {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((response) => response.json());
+  return message.body;
+});
+
+export const greetingSlice = createSlice({
+  name: 'greeting',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getMessage.fulfilled]: (state, action) => ({ ...state, greeting: action.payload }),
+  },
+});
+
+export default greetingSlice.reducer;
